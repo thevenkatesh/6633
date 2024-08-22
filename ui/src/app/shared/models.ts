fa9329b23287c37383cab3abb1d486d6bf008855
Blake Pettersson <blake.pettersson@gmail.com>
2024-08-22 13:58:12 +0200

    chore: please linters
    Signed-off-by: Blake Pettersson <blake.pettersson@gmail.com>

--- ui/src/app/shared/models.ts
+++ ui/src/app/shared/models.ts
@@ -550,6 +550,7 @@ export interface Repository {
     enableLfs?: boolean;
     githubAppId?: string;
     forceHttpBasicAuth?: boolean;
+    insecureHttpOnly?: boolean;
     enableOCI: boolean;
 }
 
