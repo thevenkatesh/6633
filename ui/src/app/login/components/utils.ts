774efe28995dcdc45cec4e4f70bb7a195a6c7432
zdp886 <austin.gloyna@capitalone.com>
2024-09-13 08:24:13 -0500

    Attempting to force base href from AbsURL function
    Signed-off-by: zdp886 <austin.gloyna@gmail.com>

--- ui/src/app/login/components/utils.ts
+++ ui/src/app/login/components/utils.ts
@@ -154,7 +154,7 @@ export const pkceCallback = async (queryParams: string, oidcConfig: AuthSettings
         throw new PKCELoginError('No token in response');
     }
 
-    document.cookie = `argocd.token=${result.id_token}; path=${requests.toAbsURL('')}`;
+    document.cookie = `argocd.token=${result.id_token}; path=${requests.toAbsURL(null)}`;
 
     window.location.replace(requests.toAbsURL('/applications'));
 };
